9a4b9c5cecddfad9bb8ed59296740081bc0b7812
esteloabell <manitobaaa@gmail.com>
2021-09-15 14:38:48 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, {useState, useEffect} from 'react';
 import { commerce } from './lib/commerce';
-import { Products, Navbar } from './components';
+import { Products, Navbar, Cart } from './components';
 import './App.css';
 
 const App = () => {
   const [productData, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
 
   // useing async function it like using .then and .catch bu the code looks much cleaner
   const fetchProducts = async () => {
@@ -18,19 +19,37 @@ const App = () => {
     setProducts(data);
   }
 
+  const fetchCart = async () => {
+    // const cart = await commerce.cart.retrieve();
+
+    // setCart(cart);
+    // same as above but shorter
+    setCart(await commerce.cart.retrieve());
+  }
+
+  const handleAddToCart = async (productId, quantity) => {
+    const item = await commerce.cart.add(productId, quantity);
+
+    setCart(item.cart);
+  };
+
   //this useEffect hook will its dependency array set to empty whichs means it will run a the start, at the render.
   // in class based component we would have used componentDidMount
   useEffect(() => {
     // inside here we can use that function call to fetch the products
     fetchProducts();
+    fetchCart();
   }, [])
 
-  console.log(productData);
+  console.log(cart);
 
   return (
     <div className="App">
-      <Navbar />
-      <Products productData={productData} />
+      <Navbar totalItems={cart.total_items} />
+      {/* <Products productData={productData} onAddToCart={handleAddToCart} /> */}
+
+      {/* were passing the cart that contains all the items in the cart as a prop to Cart component */}
+      <Cart cart={cart} />
     </div>
   );
 }
