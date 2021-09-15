dda39d19d092b56a841fc22f1d4e951ff312374f
esteloabell <manitobaaa@gmail.com>
2021-09-15 14:38:40 -0500

    Update ProductProp.js

--- src/components/Products/Product/ProductProp.js
+++ src/components/Products/Product/ProductProp.js
@@ -4,25 +4,31 @@ import { AddShoppingCart } from '@material-ui/icons';
 
 import useStyles from './styles';
 
-const Product = ({ product }) => {
+const Product = ({ product, onAddToCart }) => {
   const classes = useStyles();
+
+  const handleAddToCart = () => onAddToCart(product.id, 1);
+
+  // console.log(product);
+
+  // return <div>Test</div>
 
   return (
     <Card className={classes.root}>
-      <CardMedia className={classes.media} image={product.image} title={product.name} />
+      <CardMedia className={classes.media} image={product.media.source} title={product.name} />
       <CardContent>
         <div className={classes.cardContent}>
           <Typography gutterBottom variant="h5" component="h2">
             {product.name}
           </Typography>
           <Typography gutterBottom variant="h5" component="h2">
-            $ {product.price.formatted}
+            {product.price.formatted_with_symbol}
           </Typography>
         </div>
         <Typography dangerouslySetInnerHTML={{ __html: product.description }} variant="body2" color="textSecondary" component="p" />
       </CardContent>
       <CardActions disableSpacing className={classes.cardActions}>
-        <IconButton aria-label="Add to Cart">
+        <IconButton aria-label="Add to Cart" onClick={handleAddToCart}>
           <AddShoppingCart />
         </IconButton>
       </CardActions>
@@ -31,39 +37,3 @@ const Product = ({ product }) => {
 };
 
 export default Product;
-
-
-// hard coding
-// import React from 'react';
-// import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
-// import { AddShoppingCart } from '@material-ui/icons';
-
-// import useStyles from './styles';
-
-// const Product = ({ product }) => {
-//   const classes = useStyles();
-
-//   return (
-//     <Card className={classes.root}>
-//       <CardMedia className={classes.media} image={product.image} title={product.name} />
-//       <CardContent>
-//         <div className={classes.cardContent}>
-//           <Typography gutterBottom variant="h5" component="h2">
-//             {product.name}
-//           </Typography>
-//           <Typography gutterBottom variant="h5" component="h2">
-//             $ {product.price.formatted}
-//           </Typography>
-//         </div>
-//         <Typography dangerouslySetInnerHTML={{ __html: product.description }} variant="body2" color="textSecondary" component="p" />
-//       </CardContent>
-//       <CardActions disableSpacing className={classes.cardActions}>
-//         <IconButton aria-label="Add to Cart">
-//           <AddShoppingCart />
-//         </IconButton>
-//       </CardActions>
-//     </Card>
-//   );
-// };
-
-// export default Product;
