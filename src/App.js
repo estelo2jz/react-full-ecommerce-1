20b635b7190084fae61d8c9928a6b66853c4dbad
esteloabell <manitobaaa@gmail.com>
2021-09-16 13:59:12 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import { commerce } from './lib/commerce';
-import { Products, Navbar, Cart } from './components';
+import { Products, Navbar, Cart, Checkout } from './components';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 const App = () => {
@@ -28,10 +29,28 @@ const App = () => {
   }
 
   const handleAddToCart = async (productId, quantity) => {
-    const item = await commerce.cart.add(productId, quantity);
+    const response = await commerce.cart.add(productId, quantity);
 
-    setCart(item.cart);
+    setCart(response.cart);
+
+    // using destructuring
+    // const {cart} = await commerce.cart.add(productId, quantity);
+
+    // setCart(cart);
   };
+  const handleUpdateCartQty = async (productId, quantity) => {
+    // we put the quantity in a new object because quantity is one of the things we want to update
+    const response = await commerce.cart.update(productId, {quantity});
+    setCart(response.cart);
+  }
+  const handleRemoveFromCart = async (productId) => {
+    const response = await commerce.cart.remove(productId);
+    setCart(response.cart);
+  }
+  const handleEmptyCart = async (productId) => {
+    const response = await commerce.cart.empty();
+    setCart(response.cart);
+  }
 
   //this useEffect hook will its dependency array set to empty whichs means it will run a the start, at the render.
   // in class based component we would have used componentDidMount
@@ -44,13 +63,29 @@ const App = () => {
   console.log(cart);
 
   return (
-    <div className="App">
-      <Navbar totalItems={cart.total_items} />
-      {/* <Products productData={productData} onAddToCart={handleAddToCart} /> */}
+    <Router>
+      <div className="App">
+        <Navbar totalItems={cart.total_items} />
+        <Switch>
+          <Route exact path="/">
+            <Products productData={productData} onAddToCart={handleAddToCart} />
+          </Route>
+          <Route exact path="/cart">
+            {/* were passing the cart that contains all the items in the cart as a prop to Cart component */}
+            <Cart 
+              cart={cart} 
+              handleUpdateCartQty={handleUpdateCartQty}
+              handleRemoveFromCart={handleRemoveFromCart}
+              handleEmptyCart={handleEmptyCart}
+            />
+          </Route>
+          <Route exact path="/checkout">
+            <Checkout cart={cart} />
+          </Route>
+        </Switch>
 
-      {/* were passing the cart that contains all the items in the cart as a prop to Cart component */}
-      <Cart cart={cart} />
-    </div>
+      </div>
+    </Router>
   );
 }
 
