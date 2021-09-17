a4d1fad00030b1152de4be44d85930ca66371e5f
esteloabell <manitobaaa@gmail.com>
2021-09-17 14:18:46 -0500

    Update styles.js

--- src/components/Cart/styles.js
+++ src/components/Cart/styles.js
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 export default makeStyles((theme) => ({
   toolbar: theme.mixins.toolbar,
   title: {
-    marginTop: '5%',
+    marginTop: '10%',
   },
   emptyButton: {
     minWidth: '150px',
@@ -23,6 +23,7 @@ export default makeStyles((theme) => ({
   cardDetails: {
     display: 'flex',
     marginTop: '10%',
+    marginBottom: '10%',
     width: '100%',
     justifyContent: 'space-between',
   },
