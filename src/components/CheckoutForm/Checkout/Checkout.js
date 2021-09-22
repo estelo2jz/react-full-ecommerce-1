feba62f6d33e33a0747a665af5833886438c240a
esteloabell <manitobaaa@gmail.com>
2021-09-22 14:03:17 -0500

    added payments options

--- src/components/CheckoutForm/Checkout/Checkout.js
+++ src/components/CheckoutForm/Checkout/Checkout.js
@@ -9,7 +9,7 @@ import useStyles from './styles'
 
 const steps = ['Shipping address', 'Payment details']
 
-const Checkout = ({cart}) => {
+const Checkout = ({cart, order, onCaptureCheckout, error}) => {
   const [activeStep, setActiveStep] = useState(0);
   const [checkoutToken, setCheckoutToken] = useState(null);
   const [shippingData, setShippingData] = useState({});
@@ -48,7 +48,7 @@ const Checkout = ({cart}) => {
 
   const Form = () => activeStep === 0 
     ? <AddressForm checkoutToken={checkoutToken} next={next} /> 
-    : <PaymentForm shippingData={shippingData} checkoutToken={checkoutToken} /> 
+    : <PaymentForm shippingData={shippingData} checkoutToken={checkoutToken} backStep={backStep} nextStep={nextStep} onCaptureCheckout={onCaptureCheckout} />
 
     
   return (
