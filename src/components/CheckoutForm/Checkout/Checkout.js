632d21001cbf86c30b2c0fa5ceee9aa5e733494f
esteloabell <manitobaaa@gmail.com>
2021-09-23 11:48:53 -0500

    added stripe payment configurations

--- src/components/CheckoutForm/Checkout/Checkout.js
+++ src/components/CheckoutForm/Checkout/Checkout.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
-import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from '@material-ui/core';
+import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button, CssBaseline } from '@material-ui/core';
+import {Link} from 'react-router-dom';
 
 import { commerce } from '../../../lib/commerce';
 import AddressForm from '../AddressForm'
@@ -23,8 +24,9 @@ const Checkout = ({cart, order, onCaptureCheckout, error}) => {
         const token = await commerce.checkout.generateToken(cart.id, { type:"cart" })
         // console.log(token);
         setCheckoutToken(token);
-      } catch {
+      } catch (error) {
         // if not send an error
+        console.log(error);
       }
     }
 
@@ -40,11 +42,32 @@ const Checkout = ({cart, order, onCaptureCheckout, error}) => {
     nextStep();
   }
 
-  const Confirmation = () =>(
-    <div>
-      Confirmation
+  // if order.custumer exsit only then we wanna show the div, otherwse
+  let Confirmation = ()  => order.customer ?  (
+    <>
+      <div>
+        <Typography variant="h5" gutterBottom>Thank you for your purchase, {order.customer.firstname} {order.customer.lastname}</Typography>
+        <Divider className={classes.divider} />
+        <Typography variant="subtitle2">Order ref: {order.customer_reference}</Typography>
+      </div>
+      <br />
+      <Button component={Link} to="/" variant="outlined" type="button">Back to home</Button>
+    </>
+  ) : (
+    // show this 
+    // this simply means that, while we dont get the order then, were gonna have a spinner, because taking the order take some time
+    <div className={classes.spinner}>
+      <CircularProgress />
     </div>
   )
+
+  if (error) {
+    <>
+      <Typography variant="h5" gutterBottom>Error: {error}</Typography>
+      <br />
+      <Button component={Link} to="/" variant="outlined" type="button">Back to home</Button>
+    </>
+  }
 
   const Form = () => activeStep === 0 
     ? <AddressForm checkoutToken={checkoutToken} next={next} /> 
@@ -53,6 +76,7 @@ const Checkout = ({cart, order, onCaptureCheckout, error}) => {
     
   return (
     <>
+      <CssBaseline />
       <div className={classes.toolbar}>
         <main className={classes.layout}>
           <Paper className={classes.paper}>
